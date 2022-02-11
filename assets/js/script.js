e09fe6db6661c20de3997506563264089c777941
CMcCown <connermccown94@gmail.com>
2022-02-10 17:29:22 -0700

    add function to store arrays in localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,6 +54,7 @@ var completeEditTask = function (taskName, taskType, taskId) {
         }
     };
 
+    saveTasks();
 
     alert("Task Updated!");
     formEl.removeAttribute("data-task-id");
@@ -79,6 +80,8 @@ var createTaskEl = function(taskDataObj) {
          taskDataObj.id = taskIdCounter;
 
          tasks.push(taskDataObj);
+
+         saveTasks();
 
          var taskActionsEl = createTaskActions(taskIdCounter);
          listItemEl.appendChild(taskActionsEl);
@@ -165,6 +168,7 @@ var deleteTask = function(taskId) {
 
     // reassign tasks array to be the same as updatedTaskArr
     tasks = updatedTaskArr;
+    saveTasks();
 };
 
 var editTask = function(taskId) {
@@ -210,7 +214,13 @@ var taskStatusChangeHandler = function(event) {
             tasks[i].status = statusValue;
         }
     }
+
+    saveTasks();
 };
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
